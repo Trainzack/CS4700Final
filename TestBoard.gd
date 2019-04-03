5af9b6b73b40dd91a95cd3ba96922b4db335b986
MunozChristian110 <32554092+MunozChristian110@users.noreply.github.com>
2019-04-03 01:41:35 -0700

    Reworked Tiles

--- TestBoard.gd
+++ TestBoard.gd
@@ -3,41 +3,30 @@ extends Node
 var tile_scene = preload("res://Tile.tscn")
 
 #offsets used to populate the board. Values come from testing different positions
-var iso_x_offset = 84/2
-var iso_y_offset = -63/2
+var iso_x_offset = 83*0.7
+var iso_y_offset = 63*0.7
 var boardArray = []
 
 func _ready():
 	create_board()
-	
-	#test calls to set_cover to see how it would look
-	#probably going to rename the function to set_highlight
-	boardArray[0][0].set_cover("attack")
-	boardArray[0][1].set_cover("attack")
-	boardArray[0][2].set_cover("attack")
-	boardArray[0][3].set_cover("attack")
-	
-	boardArray[3][5].set_cover("movement")
-	boardArray[4][6].set_cover("movement")
-	boardArray[4][4].set_cover("movement")
-	boardArray[2][4].set_cover("movement")
-	boardArray[2][6].set_cover("movement")
-
 
 #Positions the tiles of the board onto the screen
 #Also fills up the boardArray with tileArrays, creating a 2d array of tiles
+#Tiles occupy z indeces 0-10
 func create_board():
 	for i in range(8):
+		var Z = 8
 		var tileArray = []
 		for j in range(8):
 			var tileNode = tile_scene.instance()
 			add_child(tileNode)
 			if i%2 == j%2:
-				tileNode.set_tile("white")
+				tileNode.set_tile_type("white")
 			else:
-				tileNode.set_tile("black")
+				tileNode.set_tile_type("black")
 				#400,400 chosen as test starting position to populate the board from
-			tileNode.position = Vector2(400 + i*iso_x_offset + j*iso_x_offset, 400 + i*iso_y_offset - j*iso_y_offset)
+			tileNode.position = Vector2(300 + j*iso_x_offset + i*iso_x_offset, 370 - j*iso_y_offset + i*iso_y_offset)
+			tileNode.z_index = Z - j
 			tileArray.append(tileNode)
 		boardArray.append(tileArray)
 
--- Tile.gd
+++ Tile.gd
@@ -1,24 +1,36 @@
-extends AnimatedSprite
+extends Area2D
+export var tyle_type = "white"
+export var highlight_type = "none"
+onready var highlight = get_node("TileSprite").get_node("Highlight")
 
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
-	pass
+func set_tile_type(type):
+	tyle_type = type
+	$TileSprite.animation = tyle_type
 
-func set_tile(type):
-	self.animation = type
-
-func set_outline(type):
-	$Outline.animation = type
-
-func set_cover(type):
-	$Cover.animation = type
+func set_highlight(type):
+	highlight_type = type
+	highlight.animation = highlight_type
+	highlight.play()
 
 func deactivate():
-	set_outline("none")
-	set_cover("none")
-	
+	set_highlight("none")
+
+func _ready():
+	pass
+
+func _input_event(viewport, event, shape_idx):
+	if event is InputEventMouseButton \
+	and event.button_index == BUTTON_LEFT \
+	and event.is_pressed():
+		self.on_click()
+
+#Will be changed to do something useful
+func on_click():
+	if highlight_type == "attack":
+		set_highlight("movement")
+	elif highlight_type == "movement":
+		set_highlight("none")
+	elif highlight_type == "none":
+		set_highlight("attack")
+	print("Click")
 #func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
